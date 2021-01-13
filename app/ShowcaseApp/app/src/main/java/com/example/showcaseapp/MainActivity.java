a3dc7a61fa088338f4062599a285f7a9d0cf2bc2
Dario Facchinetti <dario.fad@gmail.com>
2021-01-13 01:44:52 +0100

    implemented Use-Case 3: isolation of vulnerability-prone components

--- app/ShowcaseApp/app/src/main/java/com/example/showcaseapp/MainActivity.java
+++ app/ShowcaseApp/app/src/main/java/com/example/showcaseapp/MainActivity.java
@@ -2,24 +2,25 @@ package com.example.showcaseapp;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int APP_PERMISSION_REQUEST_LOCATION = 1;
+    private String[] perms = {
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.INTERNET,
+            Manifest.permission.CAMERA,
+            Manifest.permission.ACCESS_NETWORK_STATE
+    };
+
+    private PermissionUtility permsUtil;
+
     private Class[] classes = {
             UseCase1Activity.class, UseCase2Activity.class, UseCase3Activity.class
     };
@@ -28,16 +29,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (!hasLocationPermission())
-            askLocationPermission();
+        permsUtil = new PermissionUtility(this, perms);
+        if(!permsUtil.arePermissionsEnabled()){
+            permsUtil.requestMultiplePermissions();
+        }
+
     }
 
     public void goToUseCase(View view) {
         Button clickedButton = (Button) view;
         String text = clickedButton.getText().toString();
         int idx = Character.getNumericValue(text.charAt(text.indexOf("#") + 1)) - 1;
-        if (idx == 1)
-            if (!hasLocationPermission()) {
+        if (idx == 1 || idx == 2)
+            if (!permsUtil.arePermissionsEnabled()) {
                 showAdvice();
                 idx = -1;
             }
@@ -47,50 +51,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public boolean hasLocationPermission(){
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED)
-            return true;
-        else
-            return false;
-    }
-
     private void showAdvice(){
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
         alertBuilder.setCancelable(true);
-        alertBuilder.setMessage("Location permission is required to demonstrate UC#2");
+        alertBuilder.setMessage("Permissions are required to demonstrate UC#2 and UC#3");
         alertBuilder.show();
     }
 
-    public void askLocationPermission() {
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                APP_PERMISSION_REQUEST_LOCATION);
-
-    }
-
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case APP_PERMISSION_REQUEST_LOCATION: {
-                // if request is cancelled, the result arrays are empty
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission granted
-                    if (ContextCompat.checkSelfPermission(this,
-                            Manifest.permission.ACCESS_FINE_LOCATION)
-                            == PackageManager.PERMISSION_GRANTED) {
-                        // if something else has to be done with the position ...
-                    }
-
-                } else {
-                    // permission denied ...
-                }
-                return;
-            }
-
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(permsUtil.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            Log.d("Showcaseapp: ", "Permission granted");
         }
     }
 }
--- app/ShowcaseApp/app/src/main/java/com/example/showcaseapp/UseCase3Activity.java
+++ app/ShowcaseApp/app/src/main/java/com/example/showcaseapp/UseCase3Activity.java
@@ -1,11 +1,25 @@
 package com.example.showcaseapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraManager;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
-import android.os.Bundle;
+import java.util.Random;
 
-public class UseCase3Activity extends AppCompatActivity {
+public class UseCase3Activity  extends AppCompatActivity {
+
+    private StringBuilder sb;
+    private int [] some_numbers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +30,21 @@ public class UseCase3Activity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        // generating some random numbers process by the .so library
+        some_numbers = new int[10];
+        Random rand = new Random();
+        for (int i=0; i<10; i++)
+            some_numbers[i] = rand.nextInt();
+
+        // create the sb to build output
+        sb = new StringBuilder(100);
+
+        // hook method to run the UC
+        Button launch_dotso_button = (Button) findViewById(R.id.lauch_dotso_button);
+        launch_dotso_button.setText("run shared library");
+        launch_dotso_button.setOnClickListener(this::launchSharedLibrary);
+
     }
 
     @Override
@@ -23,4 +52,70 @@ public class UseCase3Activity extends AppCompatActivity {
         finish();
         return true;
     }
+
+    private void launchSharedLibrary(View v){
+        TextView dotso_textview = (TextView) findViewById(R.id.dotso_textview);
+
+        // show that the .so can access legitimate services
+        if (hasCameraPermission()){
+            sb.append("App has camera permission granted\n");
+            final CameraManager cameraManager = (CameraManager) getHandleFromJNI("CAMERA_SERVICE");
+            sb.append("The .so library can access the CameraManager via:\n" + cameraManager.toString() + "\n");
+            dotso_textview.setText(sb.toString());
+        }
+
+        // show that the .so library can do some computation
+        sb.append("\n\nThe .so can perform some linear algebra, result: " + doSomeAlgebraInJNI(some_numbers) + "\n\n");
+        dotso_textview.setText(sb.toString());
+
+        // show the confinement when policy is active
+        if (hasConnectivityPermission()) {
+            sb.append("App has connectivity permission granted\n");
+            final ConnectivityManager connectivityManager = (ConnectivityManager) getHandleFromJNI("CONNECTIVITY_SERVICE");
+            sb.append("The .so library can access the ConnectivityManager via:\n" + connectivityManager.toString() + "\n");
+            dotso_textview.setText(sb.toString());
+
+            // trying to bind the current process to network
+            boolean bound_to_network = false;
+            for (Network netw : connectivityManager.getAllNetworks()) {
+                bound_to_network = connectivityManager.bindProcessToNetwork(netw);
+                if (bound_to_network)
+                    break;
+            }
+            if (bound_to_network)
+                sb.append("Try bindProcessToNetwork: " + "SUCCESS!");
+            else
+                sb.append("Try bindProcessToNetwork: " + "FAILURE!\n(avc: denied {create} for media_d on udp_socket)");
+            dotso_textview.setText(sb.toString());
+            // clean sb
+            sb = new StringBuilder(100);
+        }
+    }
+
+    private boolean hasCameraPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED)
+            return true;
+        else
+            return false;
+    }
+
+    private boolean hasConnectivityPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_NETWORK_STATE)
+                == PackageManager.PERMISSION_GRANTED)
+            return true;
+        else
+            return false;
+    }
+
+    public native Object getHandleFromJNI(String service_name);
+
+    public native int doSomeAlgebraInJNI(int[] factors);
+
+    static {
+        System.loadLibrary("vulnerablemedia");
+    }
+
 }
